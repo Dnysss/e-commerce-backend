b209d511a86095feaa7fda7a6e29c1a3bdce1428
Dnysss <denysantos2014@hotmail.com>
2023-10-29 11:18:14 -0300

    first commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { createUserDTO } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+
+import * as bcrypt from 'bcrypt';
+import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(createUserDTO: createUserDTO): Promise<UserEntity> {
+    const crypt = 10;
+    const passwordHashed = await hash(createUserDTO.password, crypt);
+
+    return this.userRepository.save({
+      ...createUserDTO,
+      typeUser: 1,
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+}
