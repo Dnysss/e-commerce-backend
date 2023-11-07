73ea91be937a2a16ec7485fb923956baeb559944
Dnysss <denysantos2014@hotmail.com>
2023-11-07 03:23:45 -0300

    create decorator userId

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { UserId } from 'src/decorators/user-id.decorator';
 
 @Roles(UserType.User)
 @Controller('address')
@@ -12,9 +13,9 @@ export class AddressController {
         private readonly addressService: AddressService,
     ) {}
 
-    @Post('/:userId')
+    @Post()
     @UsePipes(ValidationPipe)
-    async createAdress(@Body() createAddressDto: CreateAddressDto, @Param('userId') userId: number): Promise<AddressEntity> {
+    async createAdress(@Body() createAddressDto: CreateAddressDto, @UserId('userId') userId: number): Promise<AddressEntity> {
         return this.addressService.createAddress(createAddressDto, userId)
     }
 }
