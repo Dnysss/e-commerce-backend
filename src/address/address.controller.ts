5444486b0086d0a54906a4c8d274f574691792fa
Aluno <aluno@debian>
2023-11-17 10:18:55 -0300

    create getAddress using TDD

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enum/user-type.enum';
 import { UserId } from '../decorators/user-id.decorator';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { ReturnAddressDto } from './dtos/returnAddress.dto';
 
-@Roles(UserType.User)
+@Roles(UserType.User, UserType.Admin)
 @Controller('address')
 export class AddressController {
     constructor(
@@ -17,5 +19,11 @@ export class AddressController {
     @UsePipes(ValidationPipe)
     async createAdress(@Body() createAddressDto: CreateAddressDto, @UserId('userId') userId: number): Promise<AddressEntity> {
         return this.addressService.createAddress(createAddressDto, userId);
+    }
+
+    @Get()
+    @UsePipes(ValidationPipe)
+    async findAddressByUserId(@UserId() UserId: number,): Promise<ReturnAddressDto[]> {
+        return (await this.addressService.findAddressByUserId(UserId)).map((address) => new ReturnAddressDto(address));
     }
 }
