cff79540775636436026368fb1f9a26971d6d055
Dnysss <denysantos2014@hotmail.com>
2023-11-19 19:08:46 -0300

    insert function delete product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
 import { ProductService } from './product.service';
 import { ReturnProduct } from './dtos/return-product.dto';
 import { ProductEntity } from './entities/product.entity';
 import { CreateProductDTO } from './dtos/create-product.dto';
+import { DeleteResult } from 'typeorm';
 
 @Roles(UserType.Admin, UserType.User)
 @Controller('product')
@@ -23,5 +24,11 @@ export class ProductController {
     @Post()
     async createProduct(@Body() createProduct: CreateProductDTO): Promise<ProductEntity> {
         return this.productService.createProduct(createProduct)
+    }
+
+    @Roles(UserType.Admin)
+    @Delete('/:productId')
+    async deleteProduct(@Param('productId') productId: number): Promise<DeleteResult> {
+        return this.productService.deleteProduct(productId);
     }
 }
