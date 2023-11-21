3446c6e460bd7a0a975f87542b0f1fc539c4431c
Dnysss <denysantos2014@hotmail.com>
2023-11-21 16:53:12 -0300

    insert product in cart

---
+++ src/cart-product/cart-product.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CartProductEntity } from './entities/cart-product.entity';
+import { Repository } from 'typeorm';
+import { InsertCartDTO } from 'src/cart/dtos/insert-cart.dto';
+import { CartEntity } from 'src/cart/entities/cart.entity';
+
+@Injectable()
+export class CartProductService {
+    constructor(
+        @InjectRepository(CartProductEntity)
+        private readonly cartProductRepository: Repository<CartProductEntity>
+    ) {}
+
+    async verifyProductInCart(productId: number, cartId: number): Promise<CartProductEntity> {
+        const cartProduct = await this.cartProductRepository.findOne({
+            where: {
+                productId,
+                cartId
+            }
+        });
+
+        if(!cartProduct) {
+            throw new NotFoundException("Produto não existe no carrinho")
+        }
+
+        return cartProduct;
+    }
+
+    async createProdcutInCart(insertCartDTO: InsertCartDTO, cartId: number): Promise<CartProductEntity> {
+        return this.cartProductRepository.save({
+            amount: insertCartDTO.amount,
+            productId: insertCartDTO.productId,
+            cartId
+        });
+    }
+
+    async insertProductInCart(insertCartDTO: InsertCartDTO, cart: CartEntity): Promise<CartProductEntity> {
+        const cartProduct = await this.verifyProductInCart(insertCartDTO.productId, cart.id).catch(() => undefined);
+
+        if (!cartProduct) {
+            return this.createProdcutInCart(insertCartDTO, cart.id);
+        }
+
+        return this.cartProductRepository.save({
+            ...cartProduct,
+            amount: cartProduct.amount + insertCartDTO.amount
+        });
+    }
+}
