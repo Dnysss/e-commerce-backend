82390818b3c855281bb6ee59682e4a166998deb9
Dnysss <denysantos2014@hotmail.com>
2023-11-25 10:01:35 -0300

    insert amount product in categories

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -3,22 +3,36 @@ import { CategoryEntity } from './entities/category.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCategory } from './dtos/create-category.dto';
+import { ProductService } from 'src/product/product.service';
+import { ReturnCategory } from './dtos/return-category.dto';
+import { CountProduct } from 'src/product/dtos/count-product.dto';
 
 @Injectable()
 export class CategoryService {
     constructor(
         @InjectRepository(CategoryEntity)
-        private readonly categoryRepository: Repository<CategoryEntity>
+        private readonly categoryRepository: Repository<CategoryEntity>,
+        private readonly productService: ProductService
     ) {}
+    findAmountCategoryInProducts(category: CategoryEntity, countList: CountProduct[]): number {
+        const count = countList.find((itemCount) => itemCount.category_id === category.id);
 
-    async findAllCategories(): Promise<CategoryEntity[]> {
+        if (count) {
+            return count.total;
+        }
+
+        return 0;
+    }
+    async findAllCategories(): Promise<ReturnCategory[]> {
         const categories = await this.categoryRepository.find();
+
+        const count = await this.productService.countProductsByCategory();
 
         if (!categories || categories.length === 0) {
             throw new NotFoundException('Categories empty');
         }
 
-        return categories;
+        return categories.map((category) => new ReturnCategory(category, this.findAmountCategoryInProducts(category, count)));
     }
 
     async findCategoryById(categoryId: number): Promise<CategoryEntity> {
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException, forwardRef } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
 import { DeleteResult, In, Repository } from 'typeorm';
@@ -6,79 +6,97 @@ import { CreateProductDTO } from './dtos/create-product.dto';
 import { CategoryService } from '../category/category.service';
 import { UpdateProductDTO } from './dtos/update-product.dto';
 import { find } from 'rxjs';
+import { CountProduct } from './dtos/count-product.dto';
 
 @Injectable()
 export class ProductService {
-    constructor(
-        @InjectRepository(ProductEntity)
-        private readonly productRepository: Repository<ProductEntity>,
+  constructor(
+    @InjectRepository(ProductEntity)
+    private readonly productRepository: Repository<ProductEntity>,
 
-        private readonly categoryService: CategoryService,
-    ) {}
+    @Inject(forwardRef(() => CategoryService))
+    private readonly categoryService: CategoryService,
+  ) {}
 
-    async findAll(productId?: number[], isFindRelations?: boolean): Promise<ProductEntity[]> {
-        let findOptions = {};
-        
-        if (productId && productId.length > 0) {
-            findOptions = {
-                where: {
-                    id: In(productId)
-                }
-            }
-        }
+  async findAll(
+    productId?: number[],
+    isFindRelations?: boolean,
+  ): Promise<ProductEntity[]> {
+    let findOptions = {};
 
-        if (isFindRelations) {
-            findOptions = {
-                ...findOptions,
-                relations: {
-                    category: true
-                }
-            }
-        }
-
-        const products = await this.productRepository.find(findOptions);
-
-        if (!products || products.length === 0) {
-            throw new NotFoundException('Não foram encotrados produtos');
-        }
-
-        return products;
+    if (productId && productId.length > 0) {
+      findOptions = {
+        where: {
+          id: In(productId),
+        },
+      };
     }
 
-    async createProduct(createProduct: CreateProductDTO): Promise<ProductEntity> {
-        await this.categoryService.findCategoryById(createProduct.categoryId);
-
-        return this.productRepository.save({
-            ...createProduct
-        })
-    }
-    
-    async findProductById(productId: number): Promise<ProductEntity> {
-        const product = await this.productRepository.findOne({
-            where: {
-                id: productId
-            },
-        });
-
-        if (!product) {
-            throw new NotFoundException(`Id do produto: '${productId}' não foi encontrado`);
-        }
-
-        return product;
+    if (isFindRelations) {
+      findOptions = {
+        ...findOptions,
+        relations: {
+          category: true,
+        },
+      };
     }
 
-    async deleteProduct(productId: number): Promise<DeleteResult> {
-        await this.findProductById(productId);
+    const products = await this.productRepository.find(findOptions);
 
-        return this.productRepository.delete({ id: productId });
+    if (!products || products.length === 0) {
+      throw new NotFoundException('Não foram encotrados produtos');
     }
 
-    async updateProduct(updateProduct: UpdateProductDTO, productId: number): Promise<ProductEntity>{
-        const product = await this.findProductById(productId)
+    return products;
+  }
 
-        return this.productRepository.save({
-            ...product,
-            ...updateProduct,
-        })
+  async createProduct(createProduct: CreateProductDTO): Promise<ProductEntity> {
+    await this.categoryService.findCategoryById(createProduct.categoryId);
+
+    return this.productRepository.save({
+      ...createProduct,
+    });
+  }
+
+  async findProductById(productId: number): Promise<ProductEntity> {
+    const product = await this.productRepository.findOne({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(
+        `Id do produto: '${productId}' não foi encontrado`,
+      );
     }
+
+    return product;
+  }
+
+  async deleteProduct(productId: number): Promise<DeleteResult> {
+    await this.findProductById(productId);
+
+    return this.productRepository.delete({ id: productId });
+  }
+
+  async updateProduct(
+    updateProduct: UpdateProductDTO,
+    productId: number,
+  ): Promise<ProductEntity> {
+    const product = await this.findProductById(productId);
+
+    return this.productRepository.save({
+      ...product,
+      ...updateProduct,
+    });
+  }
+
+  async countProductsByCategory(): Promise<CountProduct[]> {
+    return this.productRepository
+      .createQueryBuilder('product')
+      .select('product.category_id, COUNT(*) as total')
+      .groupBy('product.category_id')
+      .getRawMany();
+  }
 }
